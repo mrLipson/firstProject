69e6848b2151d733d7199850b2828236d11e4192
Gubarev <gubarev.a.m@yandex.com>
2022-05-19 13:32:02 +0400

    first task on course

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 "use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    coutn: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов', 'Сумерки'),
+      b = prompt('На сколько оцените его?', '-100500'),
+      c = prompt('Один из последних просмотренных фильмов', 'Сумерки'),
+      d = prompt('На сколько оцените его?', '-100500');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+
+console.log(personalMovieDB);
+
 
 
 
