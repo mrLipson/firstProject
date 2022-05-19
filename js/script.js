e061e61d7cd63a3f30677bd650170cce3a54aafa
Gubarev <gubarev.a.m@yandex.com>
2022-05-19 14:43:44 +0400

    second task on course

--- js/script.js
+++ js/script.js
@@ -24,4 +24,49 @@ console.log(personalMovieDB);
 
 
 
+console.log( NaN || 2 || undefined ); //2
+ 
+console.log( NaN && 2 && undefined ); //nan
+ 
+console.log( 1 && 2 && 3 ); // 3
+ 
+console.log( !1 && 2 || !3 ); //false
+ 
+console.log( 25 || null && !3 ); //25
+ 
+console.log( NaN || null || !3 || undefined || 5); //5
+ 
+console.log( NaN || null && !3 && undefined || 5); //5
+ 
+console.log( 5 === 5 && 3 > 1 || 5); //true
+
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger === 3 && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger || cola || fries === 3 || nuggets) {
+   console.log('Done!')
+}
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+} else console.log('error');
+
+
 
