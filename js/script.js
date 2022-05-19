a3372b6b678ff502ad244dd3e61916aaba43790f
Gubarev <gubarev.a.m@yandex.com>
2022-05-19 12:34:54 +0400

    clean file script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,5 @@
 "use strict";
 
-let firstNamePerson = 'Pasha';
-let lastNamePerson = 'Klimenko';
-let agePerson = 30;
 
-const arr = [firstNamePerson, lastNamePerson, agePerson];
 
-console.log(arr);
 
-const business = {
-    storeName: 'Bulochki',
-    timeWork: {
-        pn: '09-22',
-        vt: '09-22',
-        sr: '09-22',
-        cht: '09-22',
-        pt: '09-22',
-        sb: '10-20',
-        vs: '10-20'
-    },
-    menu: ['coockie', 'coffi', 'tea', 'buter'],
-    open: true
-};
-
-console.log(business.timeWork);
